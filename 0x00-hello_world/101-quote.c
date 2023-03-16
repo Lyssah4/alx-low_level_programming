8a73680776537d68ed5497e745b296e3a9772581
lisa Isimbi <isimbilyssah003@gmail.com>
2023-03-16 11:34:13 -0700

    hellow world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - the main function
  *
- * Return: return 0 when successful
+ * Return: return 0 when success
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
 	return (1);
 }
