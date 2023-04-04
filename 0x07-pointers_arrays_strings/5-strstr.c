c3a651331027750a6bfecbef8b59e9645d3621e4
lisa Isimbi <isimbilyssah003@gmail.com>
2023-04-04 04:00:34 -0700

    0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strstr - Entry point
+ * @haystack: the input
+ * @needle: the second input
+ * Return: 0 when successful
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for(; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
+
+	while (*l == *p && *p != '\0')
+	{
+		l++;
+		p++;
+	}
+	
+	if (*p == '\0')
+		return (haystack);
+	}
+	return (0);
+}
