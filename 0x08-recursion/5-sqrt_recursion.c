300d9b94f115e632796e15568a8a9acbe0d00765
lisa Isimbi <isimbilyssah003@gmail.com>
2023-04-05 12:49:38 -0700

    0x08-recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,7 +2,7 @@
 int actual_sqrt_recursion(int n, int i);
 
  /**
-  * _sqrt_recursion - gives the square root of a number
+  * _sqrt_recursion - returns the natural square root of a number
   * @n: number to find the square root for
   * Return: the square root
   */
