e1f8295642785078edcc3b6f6af6d61487f0af6f
lisa Isimbi <isimbilyssah003@gmail.com>
2023-04-10 08:51:21 -0700

    static libraries

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
-
 /**
  * _isupper - uppercase letters
  * @c: char to check
+ *
  * Return: 0 or 1
  */
 int _isupper(int c)
@@ -10,5 +10,5 @@ int _isupper(int c)
 	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
-		return (0);
+	return (0);
 }
--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,14 +1,15 @@
 #include "main.h"
-
 /**
- * _strchr - entry point
+ * _strchr - Entry point
  * @s: input
  * @c: input
- * Return: 0 on success
+ * Return: Always 0 (Success)
  */
-char * _strchr(char *s, char c)
+char *_strchr(char *s, char c)
 {
 	int i = 0;
+
+
 	for (; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
