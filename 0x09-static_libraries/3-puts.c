55258aad795b352f427d1e0a42859aee3ab76999
lisa Isimbi <isimbilyssah003@gmail.com>
2023-04-10 12:24:40 -0700

    libraries

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,10 +1,8 @@
-#include "main.h"
-void _puts(char *str)
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - the main function
  *
- * Return: Always 0 (Success)
+ * Return: the program will end with value zero
  */
 int main(void)
 {
