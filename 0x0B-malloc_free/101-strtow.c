a5c3a5eafb58cd4cd80c6a2859764409ba3f8cfd
lisa Isimbi <isimbilyssah003@gmail.com>
2023-04-11 14:49:43 -0700

    malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,29 +8,60 @@
  *   * Return: number of words
  *    */
 int count_word(char *s)
+		
 {
-	int flag, c, w;
+		
+		int flag, c, w;
+		
 
- 		flag = 0;
-			
-			w = 0;
+		
+			flag = 0;
+		
+				w = 0;
 
-				for (c = 0; s[c] != '\0'; c++)
+
+
+					for (c = 0; s[c] != '\0'; c++)
 
 							{
+
 										if (s[c] == ' ')
 
-	flag = 0;
+														flag = 0;
 
-													else
+												else if (flag == 0)
 
-if (flag == 0)
+															{
 
-		{
-							flag = 1;
+																flag = 1;
 
-										w++;
+																w++;
 
-		}
-							}	
+															}
+		
+							}
+
+						return (w);
+
+}
+/**
+ *  **strtow - splits a string into words
+ *
+ *  Return: pointer to an array of strings (Success)
+ *  or NULL (Error)
+ */
+char **strtow(char *str)
+
+{
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
+
+	while (*(str + len))
+		len++;
+	words = count_word(str);
+	if(words == 0)
+		return (NULL);
+
+	matrix = (char**) malloc(sizeof(char*)*(words + 1));
+	if (matrix == NULL)
 }
