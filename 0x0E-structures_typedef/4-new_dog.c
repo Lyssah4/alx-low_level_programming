edc1cbd4c718fa9aeae8cd3b060de9b2f26baad8
lisa Isimbi <isimbilyssah003@gmail.com>
2023-04-18 10:24:54 -0700

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,25 +3,31 @@
 
 /**
  * _strlen - returns the length of a string
- * @s: string to evluate
+ * @s: string to evaluate
+ *
  * Return: the length of the string
  */
 int _strlen(char *s)
 {
 	int i;
-	
+
 	i = 0;
 
 	while (s[i] != '\0')
 	{
 		i++;
 	}
+
 	return (i);
 }
+
 /**
  * *_strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
  * @dest: pointer to the buffer in which we copy the string
  * @src: string to be copied
+ *
  * Return: the pointer to dest
  */
 char *_strcpy(char *dest, char *src)
@@ -34,6 +40,7 @@ char *_strcpy(char *dest, char *src)
 	{
 		len++;
 	}
+
 	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
@@ -42,12 +49,14 @@ char *_strcpy(char *dest, char *src)
 
 	return (dest);
 }
+
 /**
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- * Return: pointer to the new dog on success, NULL on failure
+ *
+ * Return: pointer to the new dog (Success), NULL otherwise
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
