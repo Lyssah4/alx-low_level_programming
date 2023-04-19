6f301bfd4d5018c98702ce17c4bc966359e555a1
lisa Isimbi <isimbilyssah003@gmail.com>
2023-04-19 11:28:23 -0700

    update

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -24,15 +24,17 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(2);
 	}
+
 	arr = (char *)main;
 
 	for (i = 0; i < bytes; i++)
 	{
-		if(i == bytes - 1)
+		if (i == bytes - 1)
 		{
 			printf("%02hhx\n", arr[i]);
 			break;
 		}
+
 		printf("%02hhx", arr[i]);
 	}
 	return (0);
